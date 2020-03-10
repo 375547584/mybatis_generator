bfd98d2abd2b3106e3f9612d8c3f8e36fd739b68
375547584 <375547584@qq.com>
2020-03-10 17:12:17 +0800

    初始化

---
+++ src/main/java/mybatis_generator/Generator.java
@@ -0,0 +1,26 @@
+package mybatis_generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generator {
+
+    public static void main( String[] args ) throws Exception {
+        List<String> warnings = new ArrayList<>();
+        URL url = ClassLoader.getSystemResource("generatorConfig.xml");
+        File configFile = new File(url.getFile());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+}
